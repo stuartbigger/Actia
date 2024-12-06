4d17f5c8fce8fda9840c4c0c7f28638d8fe1d847
stuartbigger <189371045+stuartbigger@users.noreply.github.com>
2024-12-06 22:49:18 +0530

    Added scaffold-eth2 scripts

---
+++ contracts/script/Deploy.s.sol
@@ -0,0 +1,37 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "../src/Counter.sol";
+import "./DeployHelpers.s.sol";
+
+contract DeployScript is ScaffoldETHDeploy {
+  error InvalidPrivateKey(string);
+
+  function run() external {
+    uint256 deployerPrivateKey = setupLocalhostEnv();
+    if (deployerPrivateKey == 0) {
+      revert InvalidPrivateKey(
+        "You don't have a deployer account. Make sure you have set DEPLOYER_PRIVATE_KEY in .env or use `yarn generate` to generate a new random account"
+      );
+    }
+    vm.startBroadcast(deployerPrivateKey);
+
+    Counter counter = new Counter();
+    console.logString(
+      string.concat(
+        "Counter deployed at: ", vm.toString(address(counter))
+      )
+    );
+
+    vm.stopBroadcast();
+
+    /**
+     * This function generates the file containing the contracts Abi definitions.
+     * These definitions are used to derive the types needed in the custom scaffold-eth hooks, for example.
+     * This function should be called last.
+     */
+    exportDeployments();
+  }
+
+  function test() public { }
+}
