a2e4fc7c374c01c605416ba70bdec0163b3e243b
BigHead1599 <bighead1599@proton.me>
2024-12-29 18:38:27 +0530

    Contract read calls

--- mini-app/src/app/application/page.tsx
+++ mini-app/src/app/application/page.tsx
@@ -1,37 +1,25 @@
 "use client";
 
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { Play, Pause, Send } from "lucide-react";
-
-const track = {
-  id: 1,
-  week: "Week 12",
-  track1: {
-    title: "Hotel California",
-    artist: "Eagles",
-    votes: 1200,
-  },
-  track2: {
-    title: "Let It Be",
-    artist: "The Beatles",
-    votes: 1500,
-  },
-};
+import { useRouter, useSearchParams } from "next/navigation";
+import { ChallengeType, getChallenge } from "@/util/contractCalls";
 
 export default function SpotifyStyleTelegramMusicBetting() {
-  const [selectedTrack, setSelectedTrack] = useState<
-    "track1" | "track2" | null
-  >(null);
+  const [selectedTrack, setSelectedTrack] = useState<number>(-1);
+  const [challenge, setChallenge] = useState<ChallengeType>();
   const [betAmount, setBetAmount] = useState<string>("0.2");
   const [messages, setMessages] = useState<string[]>([]);
-  const [playingTrack, setPlayingTrack] = useState<"track1" | "track2" | null>(
-    null
-  );
+  const [playingTrack, setPlayingTrack] = useState<number>(-1);
+
+  const router = useRouter();
+
+  const id = useSearchParams().get("id");
 
   const audioRef = useRef<HTMLAudioElement>(null);
 
-  const togglePlay = (trackKey: "track1" | "track2") => {
-    setPlayingTrack(playingTrack === trackKey ? null : trackKey);
+  const togglePlay = (trackKey: number) => {
+    setPlayingTrack(playingTrack === trackKey ? -1 : trackKey);
     if (audioRef.current) {
       if (audioRef.current.paused) {
         audioRef.current.play();
@@ -42,45 +30,58 @@ export default function SpotifyStyleTelegramMusicBetting() {
   };
 
   const handleBet = () => {
-    if (selectedTrack && betAmount) {
-      const selected = track[selectedTrack];
-      const message = `You bet ${betAmount} BNB on "${selected.title}" by ${selected.artist}`;
+    if (challenge && selectedTrack && betAmount) {
+      const selected = challenge.tracks[selectedTrack];
+      const message = `You bet ${betAmount} BNB on "${selected.trackName}" by ${selected.artist}`;
       setMessages([...messages, message]);
-      setSelectedTrack(null);
+      setSelectedTrack(-1);
       setBetAmount("0.2");
     }
   };
 
+  useEffect(() => {
+      async function init() {
+          if (!id) {
+              router.push("/");
+              return;
+          }
+          const challenge = await getChallenge(BigInt(id));
+          setChallenge(challenge);
+      }
+      init();
+  }, []);
   return (
     <div className="flex flex-col lg:flex-row h-screen bg-[#121212] text-white font-sans">
       <div className="bg-[#181818] text-white w-full lg:w-[300px] p-6 flex flex-col space-y-6 border-r-2 border-[#333333]">
         <h1 className="text-4xl font-bold text-[#1DB954] text-shadow-md">
           ACTIA Music Betting
         </h1>
+        {challenge && challenge.tracks.length > 0 ? (
+        <div>
         <h2 className="text-xl text-gray-300 mt-2">Select a Track to Bet On</h2>
-        {["track1", "track2"].map((trackKey) => (
+        {challenge?.tracks.map((track) => (
           <div
-            key={trackKey}
+            key={track.id}
             className={`flex items-center justify-between p-4 rounded-lg transition-all duration-300 ease-in-out hover:bg-[#3E3E3E] cursor-pointer ${
-              selectedTrack === trackKey
+              selectedTrack === Number(track.id)
                 ? "bg-[#1DB954] text-black"
                 : "bg-[#282828] text-white"
             }`}
-            onClick={() => setSelectedTrack(trackKey as "track1" | "track2")}
+            onClick={() => setSelectedTrack(Number(track.id))}
           >
             <div className="flex items-center space-x-4">
               <button
                 onClick={(e) => {
                   e.stopPropagation();
-                  togglePlay(trackKey as "track1" | "track2");
+                  togglePlay(Number(track.id));
                 }}
                 className={`w-12 h-12 text-black rounded-full flex items-center justify-center transition-all duration-300 ${
-                  selectedTrack === trackKey
+                  selectedTrack === Number(track.id)
                     ? "bg-[#1ed760]"
                     : "bg-[#1DB954] hover:bg-[#1ed760]"
                 }`}
               >
-                {playingTrack === trackKey ? (
+                {playingTrack === Number(track.id) ? (
                   <Pause className="w-6 h-6" />
                 ) : (
                   <Play className="w-6 h-6" />
@@ -88,25 +89,28 @@ export default function SpotifyStyleTelegramMusicBetting() {
               </button>
               <div className="flex flex-col">
                 <p className="font-semibold text-lg">
-                  {track[trackKey as "track1" | "track2"].title}
+                  {track.trackName}
                 </p>
                 <p className="text-sm opacity-75">
-                  {track[trackKey as "track1" | "track2"].artist}
+                  {track.artist}
                 </p>
                 <p
-                  className={`text-sm ${selectedTrack === trackKey ? "text-black" : "text-gray-400"}`}
+                  className={`text-sm ${selectedTrack === Number(track.id) ? "text-black" : "text-gray-400"}`}
                 >
-                  Votes: {track[trackKey as "track1" | "track2"].votes}
+                  Votes: {
+                      // TODO: Get votes
+                      "0"
+                  }
                 </p>
               </div>
             </div>
             <button
               onClick={(e) => {
                 e.stopPropagation();
-                setSelectedTrack(trackKey as "track1" | "track2");
+                setSelectedTrack(Number(track.id));
               }}
               className={`px-4 py-2 text-black rounded-full text-xs font-medium transition-colors ${
-                selectedTrack === trackKey
+                selectedTrack === Number(track.id)
                   ? "bg-[#1ed760]"
                   : "bg-[#1DB954] hover:bg-[#1ed760]"
               }`}
@@ -115,6 +119,10 @@ export default function SpotifyStyleTelegramMusicBetting() {
             </button>
           </div>
         ))}
+        </div>
+        ) : 
+        <h2 className="text-xl text-gray-300 mt-2">No tracks in challenge yet</h2>
+        }
         <audio ref={audioRef}>
           <source
             src="https://ipfs.io/ipfs/QmQ7gKRNTQVZ1RVodK2nmFfGp8jsi1nmeDNzNBM3XbphGD/adiye.mp3"
@@ -135,7 +143,7 @@ export default function SpotifyStyleTelegramMusicBetting() {
           ))}
         </div>
 
-        {selectedTrack && (
+        {selectedTrack !== -1 && (
           <div className="flex items-center space-x-4 bg-[#282828] p-4 rounded-lg">
             <input
               type="number"
